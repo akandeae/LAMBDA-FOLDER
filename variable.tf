2f40b51f83204a2b2a74d0df88e0dcbf758a73a0
akandeae <104216596+akandeae@users.noreply.github.com>
2023-09-16 23:34:20 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,19 @@
-variable "source_file" {
-    default = "zip-files.zip"
-}
+//variable "source_file" {
+  //  default = "zip-files.zip"
+//}
 
-variable "filename" {
-    default = "zip-files.zip"
-}
+//variable "filename" {
+//    default = "zip-files.zip"
+//}
 
-variable "handler" {
-    default = "zip-files.zip"
-}
+//variable "handler" {
+  //  default = "zip-files.zip"
+//}
 
-variable "runtime" {
-    default = "nodejs14.x"
-}
+//variable "runtime" {
+  //  default = "nodejs14.x"
+//}
 
-variable "output_path" {
-    default = "zip-files.zip"
-}
+//variable "output_path" {
+  //  default = "zip-files.zip"
+//}
