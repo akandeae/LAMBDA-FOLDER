4bca033cf238f56e4e49a53cc151ed0633961455
akandeae <104216596+akandeae@users.noreply.github.com>
2023-09-16 23:32:22 -0500

    Update lambda.tf
    return back to the previous commit/ delete variable

--- lambda.tf
+++ lambda.tf
@@ -18,21 +18,21 @@ resource "aws_iam_role" "iam_for_lambda" {
 
 data "archive_file" "lambda" {
   type        = "zip"
-  source_file = "var.source_file"
-  output_path = "var.output_path"
+  source_file = "lambda-function.py"
+  output_path = "zip-files.zip"
 }
 
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "var.filename"
+  filename      = "zip-files.zip"
   function_name = "lambda-function"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "var.handler"
+  handler       = "index.test"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
-  runtime = "var.runtime"
+  runtime = "nodejs14.x"
 
   environment {
     variables = {
