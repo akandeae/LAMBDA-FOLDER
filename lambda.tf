2c241404ff11c7fed995a4f6934d606c226228d8
akandeae <104216596+akandeae@users.noreply.github.com>
2023-09-17 22:23:01 -0500

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -18,7 +18,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 
 data "archive_file" "lambda" {
   type        = "zip"
-  source_file = "var.source_file"
+  source_file = "lambda-function.py"
   output_path = "zip-files.zip"
 }
 
