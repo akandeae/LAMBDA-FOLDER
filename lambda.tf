cb72ca66942d5c4dbd6aa69db0fe63125ffaf96a
akandeae <104216596+akandeae@users.noreply.github.com>
2023-09-17 22:05:54 -0500

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -26,7 +26,7 @@ resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
   filename      = "zip-files.zip"
-  function_name = "var.function_name"
+  function_name = "lambda-function"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "var.handler"
 
