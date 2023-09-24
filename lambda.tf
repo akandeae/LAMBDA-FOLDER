773c1962f5fa5a27e0fb86f2181b9f892f99ad27
akandeae <akande.ae@gmail.com>
2023-09-23 22:35:57 -0500

    update

--- lambda.tf
+++ lambda.tf
@@ -11,6 +11,13 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 
+
+
+locals {
+  file_location = "output/zip-files.zip"
+}
+
+
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
@@ -19,13 +26,13 @@ resource "aws_iam_role" "iam_for_lambda" {
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "lambda-function.py"
-  output_path = "zip-files.zip"
+  output_path = "${local.file_location}"
 }
 
 resource "aws_lambda_function" "test_lambda" {
   # If the file is not in the current working directory you will need to include a
   # path.module in the filename.
-  filename      = "zip-files.zip"
+  filename      = "${local.file_location}"
   function_name = "demo-lambda"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda-function.lambda_handler"
