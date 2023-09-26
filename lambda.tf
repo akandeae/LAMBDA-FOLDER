1bfcd1cb28a1901a5a04a56c5066188aaa161404
akandeae <akande.ae@gmail.com>
2023-09-26 07:45:11 -0500

    iam added

--- lambda.tf
+++ lambda.tf
@@ -1,27 +1,9 @@
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-
 
 locals {
   file_location = "output/zip-files.zip"
 }
 
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
 
 data "archive_file" "lambda" {
   type        = "zip"
