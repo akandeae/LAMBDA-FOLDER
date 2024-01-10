5849f4dc6dd067cdb6bb255acda7a2f38571bcc3
akandeae <104216596+akandeae@users.noreply.github.com>
2024-01-10 14:02:17 -0500

    Create provider2.tf

---
+++ provider2.tf
@@ -0,0 +1,17 @@
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+
+  # config_context = "eks-cluster-prod"
+  # config_path = "~/.kube/config"
+
+}
+
+
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.this[0].id
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.this[0].id
+}
